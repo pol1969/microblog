84b8859db3379b5d19323062bb17e4d6f649ae02
pol1969 <podberezsky1969@gmail.com>
2018-10-02 06:27:26 +0300

    ver 7 обработка ошибок и отладка

--- app/routes.py
+++ app/routes.py
@@ -78,11 +78,10 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
-
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
